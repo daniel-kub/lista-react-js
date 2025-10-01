8a6f003709a86c2292bc10214cd69d9d2f957c13
Daniel Kubowicz <159922754+daniel-kub@users.noreply.github.com>
2025-10-01 12:27:42 +0200

    Odblokowanie domeny

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',       // pozwala na dostęp spoza kontenera
+    port: 80,              // port serwera w kontenerze
+    strictPort: true,      // jeśli port jest zajęty, wywali błąd
+    hmr: {
+      host: 'lista.techrabka.eu', // domena, z której będzie się łączyć HMR
+    }
+  }
 })
